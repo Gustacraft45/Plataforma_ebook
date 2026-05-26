b3b970a82cbb0f4e0670dea0d19eac5708649184
GUSTAVO MENDES RODRIGUES <gu5168111@edu.uniube.br>
2026-05-25 21:36:27 -0300

    feat: rota de login com jwt configurada
    O usuário manda o e-mail e a senha em texto normal. O nosso backend busca o e-mail no banco, pega a senha embaralhada que está lá e usa o bcrypt para testar se a senha digitada bate com o embaralhamento. Se bater, geramos o Token JWT (o Crachá Virtual).

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User'); // Importa o molde de Usuário que criamos antes
 
 // ROTA DE CADASTRO: http://localhost:5000/api/auth/register
@@ -36,4 +37,45 @@ router.post('/register', async (req, res) => {
   }
 });
 
+// ROTA DE LOGIN: http://localhost:5000/api/auth/login
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. Procurar o usuário pelo e-mail
+    const user = await User.findOne({ email });
+    if (!user) {
+      // Retornamos um erro genérico por segurança (para não avisar se o erro foi no email ou na senha)
+      return res.status(400).json({ message: 'Credenciais inválidas.' });
+    }
+
+    // 2. Comparar a senha digitada com a senha criptografada do banco
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Credenciais inválidas.' });
+    }
+
+    // 3. Criar o Token JWT (Crachá Virtual)
+    const token = jwt.sign(
+      { userId: user._id }, // Guardamos o ID do usuário dentro do token
+      process.env.JWT_SECRET, // Assinamos com a nossa palavra secreta
+      { expiresIn: '1d' } // O token vale por 1 dia
+    );
+
+    // 4. Devolver o token e os dados básicos do usuário para o Frontend
+    res.json({
+      message: 'Login bem-sucedido!',
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        xp: user.xp
+      }
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Erro no servidor ao fazer login.' });
+  }
+});
 module.exports = router;
